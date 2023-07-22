98e81abeb4b458ab7c4d6d1956c7b1cd73076903
Juan <104234658+JuanNascimentoSouza@users.noreply.github.com>
2023-07-21 22:37:10 -0300

    pngs, slides img, button css

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 document.querySelector('video').play();
+
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide() {
+  slides.forEach((slide, index) => {
+    slide.classList.remove('active');
+    if (index === currentSlide) {
+      slide.classList.add('active');
+    }
+  });
+}
+
+function nextSlide() {
+  currentSlide++;
+  if (currentSlide >= slides.length) {
+    currentSlide = 0;
+  }
+  showSlide();
+}
+
+function prevSlide() {
+  currentSlide--;
+  if (currentSlide < 0) {
+    currentSlide = slides.length - 1;
+  }
+  showSlide();
+}
+
+setInterval(nextSlide, 3000); // Trocar de slide a cada 3 segundos (3000 ms)
